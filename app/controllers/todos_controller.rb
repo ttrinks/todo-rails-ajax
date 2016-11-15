cde6faf53455d7fe988e8efc246598dabf581cd0
ttrinks <ttrinks.git@icloud.com>
2016-11-15 18:21:11 +0100

    create.js and todo-list document added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,6 +5,7 @@ class TodosController < ApplicationController
   # GET /todos.json
   def index
     @todos = Todo.all
+    @todo = Todo.new
   end
 
   # GET /todos/1
@@ -29,6 +30,7 @@ class TodosController < ApplicationController
     respond_to do |format|
       if @todo.save
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.js   {}
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
